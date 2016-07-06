814b8d6f197e05eeefab402636db962f788a0a98
Arthur Mo <Yen50414@gmail.com>
2016-07-06 16:52:08 -0400

    Give grids height

--- src/main/java/com/audition/PacmanGrid.java
+++ src/main/java/com/audition/PacmanGrid.java
@@ -3,12 +3,18 @@ package com.audition;
 public class PacmanGrid {
 	
 	private int gridWidth;
+	private int gridHeight;
 	
-	public PacmanGrid(int width) {
+	public PacmanGrid(int width, int height) {
 		gridWidth = width;
+		gridHeight = height;
 	}
 	
 	public int getWidth() {
 		return gridWidth;
+	}
+	
+	public int getHeight() {
+		return gridHeight;
 	}
 }
--- src/test/java/com/audition/PacmanGridTest.java
+++ src/test/java/com/audition/PacmanGridTest.java
@@ -5,13 +5,18 @@ import junit.framework.*;
 public class PacmanGridTest extends TestCase {
 	
 	public void testGenerateGrid() throws Exception {
-		PacmanGrid testGrid = new PacmanGrid(5);
+		PacmanGrid testGrid = new PacmanGrid(28, 31);
 		assertNotNull(testGrid);
 	}
 	
 	public void testGridWidth() throws Exception {
-		PacmanGrid testGrid = new PacmanGrid(5);
-		assertEquals(testGrid.getWidth(), 5);
+		PacmanGrid testGrid = new PacmanGrid(28, 31);
+		assertEquals(testGrid.getWidth(), 28);
+	}
+	
+	public void testGridHeight() throws Exception {
+		PacmanGrid testGrid = new PacmanGrid(28, 31);
+		assertEquals(testGrid.getHeight(), 31);
 	}
 	
 }
