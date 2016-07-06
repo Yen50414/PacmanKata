4e57f896999a3fb496a9c41fb8cf9239347eec92
Arthur Mo <Yen50414@gmail.com>
2016-07-06 17:03:43 -0400

    Grid generates with dots

--- src/main/java/com/audition/PacmanGrid.java
+++ src/main/java/com/audition/PacmanGrid.java
@@ -5,9 +5,19 @@ public class PacmanGrid {
 	private int gridWidth;
 	private int gridHeight;
 	
+	boolean[][] grid;
+	
 	public PacmanGrid(int width, int height) {
 		gridWidth = width;
 		gridHeight = height;
+		
+		grid = new boolean[gridWidth][gridHeight];
+		
+		for(int i = 0; i < gridWidth; i++) {
+			for(int j = 0; j < gridHeight; j++) {
+				grid[i][j] = true; // true if there is a dot
+			}
+		}
 	}
 	
 	public int getWidth() {
@@ -16,5 +26,9 @@ public class PacmanGrid {
 	
 	public int getHeight() {
 		return gridHeight;
+	}
+	
+	public boolean getCell(int x, int y) {
+		return grid[x][y];
 	}
 }
--- src/test/java/com/audition/PacmanGridTest.java
+++ src/test/java/com/audition/PacmanGridTest.java
@@ -4,19 +4,30 @@ import junit.framework.*;
 
 public class PacmanGridTest extends TestCase {
 	
+	PacmanGrid testGrid;
+	
+	public void setUp() {
+		testGrid = new PacmanGrid(28, 31);
+	}
+	
 	public void testGenerateGrid() throws Exception {
-		PacmanGrid testGrid = new PacmanGrid(28, 31);
 		assertNotNull(testGrid);
 	}
 	
 	public void testGridWidth() throws Exception {
-		PacmanGrid testGrid = new PacmanGrid(28, 31);
 		assertEquals(testGrid.getWidth(), 28);
 	}
 	
 	public void testGridHeight() throws Exception {
-		PacmanGrid testGrid = new PacmanGrid(28, 31);
 		assertEquals(testGrid.getHeight(), 31);
+	}
+	
+	public void testGridFilledWithDots() throws Exception {
+		for(int i = 0; i < testGrid.getWidth(); i++) {
+			for(int j = 0; j < testGrid.getHeight(); j++) {
+				assertEquals(testGrid.getCell(i, j), true);
+			}
+		}
 	}
 	
 }
