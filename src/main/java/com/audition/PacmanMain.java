a447d27ccb67d97e2a94b5a436929f91ba33d353
Arthur Mo <Yen50414@gmail.com>
2016-07-07 19:34:01 -0400

    Wrote main class to initialize and print game to console

---
+++ src/main/java/com/audition/PacmanMain.java
@@ -0,0 +1,28 @@
+package com.audition;
+
+public class PacmanMain {
+	
+	private static PacmanGrid gameGrid;
+	
+	private static void printGrid() {
+		String eol = System.getProperty("line.separator");
+		
+		for(int i = 0; i < gameGrid.getWidth(); i++) {
+			for(int j = 0; j < gameGrid.getHeight(); j++) {
+				PacObjects curr = gameGrid.getCell(i,j);
+				if(curr == PacObjects.PACMAN) {
+					System.out.print("V");
+				} else {
+					System.out.print(".");
+				}
+			}
+			System.out.print(eol);
+		}
+	}
+
+	public static void main(String[] args) {
+		gameGrid = new PacmanGrid(28, 31);
+		printGrid();
+	}
+
+}
